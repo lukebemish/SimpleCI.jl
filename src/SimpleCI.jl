eed734a41efd0ad1b36687df75b8cca39e8f8a31
Luke Bemish <lukebemish@lukebemish.dev>
2023-04-01 17:04:50 -0500

    Ready for testing...

--- src/SimpleCI.jl
+++ src/SimpleCI.jl
@@ -11,7 +11,6 @@ end
 abstract type Step end
 StructTypes.StructType(::Type{Step}) = StructTypes.AbstractType()
 StructTypes.subtypekey(::Type{Step}) = :type
-StructTypes.subtypes(::Type{Step}) = (gradle=GradleStep,shell=ShellStep)
 
 struct GradleStep <: Step
     type::String
@@ -56,25 +55,61 @@ function runstep(step::ShellStep, env::Env)
     run(`$(step.script)`)
 end
 
+struct TagVersionStep <: Step
+    type::String
+    name::String
+    ignoreexisting::Bool
+end
+
+function StructTypes.construct(::Type{TagVersionStep}, type, name, ignoreexisting)
+    TagVersionStep(type, name, something(ignoreexisting, false))
+end
+StructTypes.StructType(::Type{TagVersionStep}) = StructTypes.Struct()
+
+name(x::TagVersionStep) = x.name
+workingdir(x::TagVersionStep) = "."
+function runstep(step::TagVersionStep, env::Env)
+    if env.version === nothing
+        error("No version found in previous steps.")
+    end
+    if success(`git rev-parse $(env.version)`)
+        if step.ignoreexisting
+            println("Version $(env.version) already exists.")
+            return
+        else
+            error("Version $(env.version) already exists.")
+        end
+    end
+    run(`git tag -a $(env.version) -m ""`)
+    run(`git push origin --tags`)
+end
+
+StructTypes.subtypes(::Type{Step}) = (gradle=GradleStep,shell=ShellStep,tagversion=TagVersionStep)
+
 struct Config
     steps::Vector{Step}
+    filter::String
 end
 
 StructTypes.StructType(::Type{Config}) = StructTypes.Struct()
+function StructTypes.construct(::Type{Config}, steps, filter)
+    Config(steps, something(filter, "(?i)^\\[no-?_?ci"))
+end
 
 function main(; configpath = "config.hrse")
     out = read(`git log -1 --pretty=%B`, String)
-    if match(r"^\[no-?_?ci"i, strip(out)) !== nothing
+    config = readhrse(joinpath(".simpleci.jl/", configpath), type = Config)
+    if match(Regex(config.filter), strip(out)) !== nothing
         println("Skipping no-ci commit.")
         return
     end
-    config = readhrse(joinpath(".simpleci.jl/", configpath), type = Config)
     rootdir = pwd()
     env = Env(nothing)
     for step in config.steps
         println("Running step $(name(step))...")
         cd(joinpath(rootdir, workingdir(step)))
         runstep(step, env)
+        cd(rootdir)
     end
 end
 
